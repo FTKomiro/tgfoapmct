cf332dd2c9ae20594deb363ff41e094f18d91f45
Furkan TMZL <156383053+FTKomiro@users.noreply.github.com>
2025-09-20 00:24:49 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("KomiroFT.PhotoModeCharacterToggler", "PhotoModeCharacterToggler", "1.0.1")]
+public class Plugin : BaseUnityPlugin
+{
+    GameObject pmct;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (pmct == null)
+                pmct = FindEnviroment();
+            if (pmct != null)
+                pmct.SetActive(!pmct.activeSelf);
+        }
+    }
+
+    GameObject FindEnviroment()
+    {
+        foreach (var go in Resources.FindObjectsOfTypeAll<GameObject>())
+        {
+            if (go.name == "Enviroment")
+                return go;
+        }
+        return null;
+    }
+}
